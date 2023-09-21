6da6bbf3ee4015d581d1c1bacc57277a2f04dc07
Peace <97194474+PeaceOffiong@users.noreply.github.com>
2023-09-21 21:48:19 +0000

    form validations

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,51 +1,58 @@
 import { useState } from "react";
 import { auth } from "../firebase/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import {useNavigate} from "react-router-dom";
-
-interface ErrorState {
-  passwordEmail: string;
-  emailError: string;
-}
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const navigate = useNavigate();
 
-  const [error, setError] = useState<ErrorState>({
-    passwordEmail: "",
-    emailError: "",
-  });
+  const [error, setError] = useState<string>("");
+  console.log(error);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user)
-      navigate("/")
-    }).catch((error) =>{
+    try {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.message);
+        });
+      setPassword("");
+      setEmail("");
+    } catch (error) {
       console.log(error);
-    });
+      setError("Please check that your internet is connected");
+    }
   };
 
   return (
     <main className="loginPage">
       <form onSubmit={handleSubmit}>
         <h1>Login </h1>
+        <p>Welcome Back</p>
+        <div className="error">{error}</div>
         <input
-          type="text"
+          type="email"
           placeholder="Enter your Email"
           value={email}
+          required
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Enter your Password"
           value={password}
+          required
           onChange={(e) => setPassword(e.target.value)}
         />
-        <input type="submit" />
+        <input type="submit" className="submit" />
       </form>
     </main>
   );
